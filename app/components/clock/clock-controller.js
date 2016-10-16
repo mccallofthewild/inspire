fe95e311a39bdc055e3aed99a3f39f5e5d11f628
McCall Alexander <mccallalexander96@gmail.com>
2016-10-16 13:08:02 -0600

    fixed time to operate in terms of military time

--- app/components/clock/clock-controller.js
+++ app/components/clock/clock-controller.js
@@ -23,7 +23,7 @@ function ClockController(){
         var militaryHour = date.getHours();
         var hour = militaryHour%12;
         var minute = date.getMinutes().toString();
-        var readableTimeOfDay = militaryHour<11? "morning" : militaryHour<5? "afternoon" : "evening"
+        var readableTimeOfDay = militaryHour<11? "morning" : militaryHour<16? "afternoon" : "evening"
         var readableTime = `${hour? hour : "12"}:${!!minute[1]? minute : "0" + minute}`;
         $('#timeofday').html(readableTimeOfDay)
         $('#time').html(readableTime);
